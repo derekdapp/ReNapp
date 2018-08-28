f075e771fa9db9a68b43be76df0ae536e0fa824d
derekdapp <derekdapp@gmail.com>
2018-08-28 15:49:05 -0600

    new style sidemenu

--- src/screens/SideMenu.js
+++ src/screens/SideMenu.js
@@ -7,6 +7,8 @@ import {
   TouchableOpacity
 } from 'react-native';
 import { ListItem } from 'react-native-elements'
+import { ThemedText } from '../components';
+import { ThemeConsumer } from '../theme';
 
 class SideMenu extends Component {
   navigateToScreen = (route) => {
@@ -16,30 +18,49 @@ class SideMenu extends Component {
 
   render () {
     return (
-      <View style={styles.container}>
-        <ScrollView>
-          <ListItem
-            title='Home'
-            leftIcon={{ name: 'home' }}
-            onPress={() => this.navigateToScreen('Home')}
-          />
-          <ListItem
-            title='Screen One'
-            leftIcon={{ name: 'cached' }}
-            onPress={() => this.navigateToScreen('ScreenOne')}
-          />
-        </ScrollView>
-        <View style={styles.footerContainer}>
-          <Text>footer power</Text>
+      <ThemeConsumer>
+      {(props) => (
+        <View style={styles.container}>
+          <View style={{ 
+            backgroundColor: props.theme.colors.primary,
+            height: 160,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+            <ThemedText 
+              h2 
+              style={{
+                color: props.theme.colors.text.inverse
+              }}
+            >
+              ReNapp
+            </ThemedText>
+          </View>
+          <ScrollView>
+            <ListItem
+              title='Home'
+              leftIcon={{ name: 'home' }}
+              onPress={() => this.navigateToScreen('Home')}
+            />
+            <ListItem
+              title='Screen One'
+              leftIcon={{ name: 'cached' }}
+              onPress={() => this.navigateToScreen('ScreenOne')}
+            />
+          </ScrollView>
+          <View style={styles.footerContainer}>
+            <Text>footer power</Text>
+          </View>
         </View>
-      </View>
+      )}
+      </ThemeConsumer>
     );
   }
 }
 
 const styles = StyleSheet.create({
     container: {
-      paddingTop: 20,
       flex: 1
     },
     navItemStyle: {
